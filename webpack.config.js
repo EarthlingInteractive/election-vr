2cf3ca5b16dcd35a1d7cb3d3f4a6ddf0cc05eb3e
Steve Brudz <sbrudz@gmail.com>
2016-11-23 19:36:04 -0600

    Install and configure yarn, babel, and web pack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	entry: './app/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: './dist'
+	},
+	devtool: "cheap-module-eval-source-map",
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}]
+	}
+};
