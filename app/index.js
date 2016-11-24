5b910a914d2574b5f809c8a837c693faaa3866fd
Steve Brudz <sbrudz@gmail.com>
2016-11-23 19:44:29 -0600

    Initial version of US map

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import * as d3 from 'd3';
+import * as topojson from 'topojson-client';
+
+const svg = d3.select("svg");
+const width = +svg.attr("width");
+const height = +svg.attr("height");
+
+const path = d3.geoPath();
+
+const ready = (error, us) => {
+	if (error) throw error;
+
+	svg.append("g")
+		.attr("class", "states")
+		.selectAll("path")
+		.data(topojson.feature(us, us.objects.states).features)
+		.enter().append("path")
+		.attr("d", path);
+};
+
+d3.queue()
+	.defer(d3.json, "./data/us-10m.v1.json")
+	.await(ready);
