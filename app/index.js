43a869abb2a3621e8e46cd3177f4463cec5e1e83
Steve Brudz <sbrudz@gmail.com>
2016-11-23 20:43:36 -0600

    Add eslint and editorconfig and peg node version with nvm

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
-import * as d3 from 'd3';
-import * as topojson from 'topojson-client';
+import * as d3 from "d3";
+import * as topojson from "topojson-client";
 
 const svg = d3.select("svg");
 const width = +svg.attr("width");
@@ -8,16 +8,17 @@ const height = +svg.attr("height");
 const path = d3.geoPath();
 
 const ready = (error, us) => {
-	if (error) throw error;
+    if (error) throw error;
 
-	svg.append("g")
-		.attr("class", "states")
-		.selectAll("path")
-		.data(topojson.feature(us, us.objects.states).features)
-		.enter().append("path")
-		.attr("d", path);
+    svg.append("g")
+        .attr("class", "states")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .enter()
+        .append("path")
+        .attr("d", path);
 };
 
 d3.queue()
-	.defer(d3.json, "./data/us-10m.v1.json")
-	.await(ready);
+    .defer(d3.json, "./data/us-10m.v1.json")
+    .await(ready);
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-	entry: './app/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: './dist'
-	},
-	devtool: "cheap-module-eval-source-map",
-	module: {
-		loaders: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: 'babel-loader'
-		}]
-	},
-	devServer: {
-		publicPath: "http://localhost:3033/dist/"
-	}
+    entry: "./app/index.js",
+    output: {
+        filename: "bundle.js",
+        path: "./dist",
+    },
+    devtool: "cheap-module-eval-source-map",
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+        }],
+    },
+    devServer: {
+        publicPath: "http://localhost:3033/dist/",
+    },
 };
