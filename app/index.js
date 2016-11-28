c109a3319b0cbd6e5014086de47702addc65816a
Steve Brudz <sbrudz@gmail.com>
2016-11-27 22:02:29 -0600

    Test out smaller scaled images of states

--- app/index.js
+++ app/index.js
@@ -2,21 +2,34 @@ import * as d3 from "d3";
 import * as topojson from "topojson-client";
 
 const svg = d3.select("svg");
-const width = +svg.attr("width");
-const height = +svg.attr("height");
 
 const path = d3.geoPath();
 
 const ready = (error, us) => {
     if (error) throw error;
 
-    svg.append("g")
+    const statesContext = svg.append("g")
         .attr("class", "states")
         .selectAll("path")
-        .data(topojson.feature(us, us.objects.states).features)
+        .data(topojson.feature(us, us.objects.states).features);
+
+    statesContext
         .enter()
         .append("path")
-        .attr("d", path);
+        .attr("id", d => `fips-${ d.id }`)
+        .attr("d", path)
+        .attr("fill", "orange");
+
+    statesContext
+        .enter()
+        .append("path")
+        .attr("id", d => `test-${ d.id }`)
+        .attr("d", path)
+        .attr("fill", "green")
+        .attr("transform", (d) => {
+            const [centerX, centerY] = path.centroid(d);
+            return `translate(${ centerX * 0.5 }, ${ centerY * 0.5 }) scale(0.5)`;
+        });
 };
 
 d3.queue()
