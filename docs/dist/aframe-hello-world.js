0837d28e43da55810938672b5085e42513a7d493
Steve Brudz <sbrudz@gmail.com>
2017-11-03 11:52:30 -0500

    Release VR election map v1 for testing

--- docs/dist/aframe-hello-world.js
+++ docs/dist/aframe-hello-world.js
@@ -1,14 +1,14 @@
-webpackJsonp([4],{
+webpackJsonp([5],{
 
-/***/ 55:
+/***/ 510:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(12);
+__webpack_require__(23);
 
 /***/ })
 
-},[55]);
+},[510]);
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hZnJhbWUtaGVsbG8td29ybGQuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQSx3QiIsImZpbGUiOiJhZnJhbWUtaGVsbG8td29ybGQuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgXCJhZnJhbWVcIjtcblxuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyAuL2FmcmFtZS1oZWxsby13b3JsZC5qcyJdLCJzb3VyY2VSb290IjoiIn0=
